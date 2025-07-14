d9665007edd7d2c8d927f41454741da505ce3fee
Dio2003 <117215815+Dio2003@users.noreply.github.com>
2025-07-14 15:20:41 -0300

    Updated server.js

--- models/server.js
+++ models/server.js
@@ -1,12 +1,1 @@
-@@ .. @@
-        this.salasR = '/api/crearsalas'; // de salas paso 4 03/06/2025
-         this.ingresarsalasR = '/api/ingresarsalas'; // de detallesalas paso 4 03/06/2025
--        this.reportesR = '/api/reportes'; // para reportes con JasperReports
-+        this.reportesR = '/api/reportes'; // para reportes con JasperStarter
-         this.dbConnection();
-@@ .. @@
-         this.app.use(this.salasR, require('./../routes/RoutesSala')); // de salas paso 4 03/06/2025
-         this.app.use(this.ingresarsalasR, require('./../routes/RoutesIngresarSala')); // de detallesalas paso 4 03/06/2025
--        this.app.use(this.reportesR, require('./../routes/reportes')); // para reportes
-+        this.app.use(this.reportesR, require('./../routes/reportes')); // para reportes con JasperStarter
-     }
+{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
